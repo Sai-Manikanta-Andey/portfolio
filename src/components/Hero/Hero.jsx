996fb99ef78e85c1fe29d322b732d6cdae5efa5d
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2023-09-15 17:14:57 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero-wrapper " id="Home">
+      <div className="profile-container innerWidth">
+        <div className="paddings innerWidth profile-innerContainer ">
+          {/* hero left side */}
+          <div className="hero-left innerWidth">
+            <div className="hero-title primaryText">
+              <h1>
+                Front-End React <br /> Developer
+              </h1>
+            </div>
+            <div className="hero-intro ">
+              <p className="secondaryText">
+                Hi, I'm Sai Manikanta Andey. A passionate react front-end
+                developer.
+              </p>
+            </div>
+            <div className="social-links">
+              <a href="https://github.com/Sai-Manikanta-Andey" target="_blank">
+                <img src="./github.png" alt="github" width={30} />
+              </a>
+              <a href="https://www.linkedin.com/in/sai-manikanta-andey/" target="_blank">
+                <img src="./linkedin.png" alt="linkedin" width={30} />
+              </a>
+            </div>
+            {/* tech stack beginning */}
+            <div className=" innerWidth tech-stack">
+              <div className="techstack-title ">
+                <h4 className="innerWidth">
+                  <span>Stack </span>
+                  <span>|</span>
+                </h4>
+              </div>
+              <div className="tech-stack-icons">
+                <img src="./html-5.png" alt="html" width={30} />
+                <img src="./css.png" alt="css" width={30} />
+                <img src="./js.png" alt="js" width={30} />
+                <img src="./react-js-icon.png" alt="react" width={30} />
+                <img src="./python.png" alt="python" width={30} />
+              </div>
+            </div>
+          </div>
+
+          {/* hero right side */}
+          <div className="hero-right innerWidth">
+            <div className=" flexCenter image-container">
+              <img src="./profile-pic.png" alt="profilepic" width={250} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/ProjectPage/Project.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import "./Project.css";
+import { FaGithub } from "react-icons/fa";
+import {HiExternalLink} from "react-icons/hi"
+
+const Project = () => {
+  const projectData = [
+    {
+      id: 1,
+      title: "Real-Estate Website ",
+      image: "./realestate.png",
+
+      projectDescription:
+        "A simple responsive website developed using react js and css. An online platform where users can see different properties to buy and rent.  ",
+      stack: [{id:1,tech:"React"}, {id:2,tech:"CSS"}],
+      liveUrl: "https://realestateweb1.netlify.app",
+      githubUrl:
+        "https://github.com/Sai-Manikanta-Andey/RealEstateWebsite-using-react",
+    },
+    {
+      id: 2,
+      title: "Nike Store",
+      image: "./nike1.png",
+      projectDescription:
+        "A simple Ecommerce store of Nike. A platform where users can buy exclusive nike special edition only sneakers.",
+      stack: [{id:1,tech:"JavaScript"}, {id:2,tech:"CSS"}],
+      liveUrl: "https://tranquil-kitten-bca2cd.netlify.app/",
+      githubUrl: "https://github.com/Sai-Manikanta-Andey/ReactDiceGameP3",
+    },
+    {
+      id: 3,
+      title: "Dice Game",
+      image: "./dicegame.png",
+      projectDescription:
+        "A fun game where you can guess a number and roll the dice, on guessing correctly you'll get points and if you guess wrong , points will be deducted.",
+      stack: [{id:1,tech:"JavaScript"}, {id:2,tech:"CSS"}],
+      liveUrl: "https://tranquil-kitten-bca2cd.netlify.app/",
+      githubUrl: "https://github.com/Sai-Manikanta-Andey/ReactDiceGameP3",
+    },
+  ];
+  return (
+    <section className="project-wrapper" id="Projects">
+      <div className="paddings innerWidth project-container">
+        <h2 className="Heading">Projects</h2>
+        {projectData.map((eachObj) => {
+          return (
+            <div className="innerContainer" key={eachObj.id}>
+              <div className="project-image alignCenter">
+                <img src={eachObj.image} alt={eachObj.title} width={350} />
+              </div>
+              <div className="project-details ">
+                <div className="project-title">
+                  <h4> {eachObj.title} </h4>
+                </div>
+                <div className="project-description secondaryText">
+                  <p> {eachObj.projectDescription} </p>
+                </div>
+                <div className="stack ">
+                  {eachObj.stack.map((eachItem) => {
+                    return <div key={eachItem.id}>{eachItem.tech}</div>;
+                  })}
+                </div>
+                <div className="innerWidth project-buttons">
+                  <div className="githubButton">
+                    <button>
+                      {" "}
+                      <a href={eachObj.githubUrl} target="_blank">
+                        <div className="buttons-inside">
+                          Github
+                          <FaGithub />
+                        </div>
+                      </a>
+                    </button>
+                  </div>
+                  <div className="liveButton">
+                    <button>
+                      {" "}
+                      <a href={eachObj.liveUrl} target="_blank">
+                        <div className="buttons-inside">
+                          Preview
+                          <HiExternalLink />
+                        </div>
+                      </a>{" "}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Project;
