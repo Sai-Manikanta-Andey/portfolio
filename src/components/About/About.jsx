bec9fbd17421d4dabe9c68ba9ef110608e2186b6
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-06-03 11:19:12 +0530

    fix : unnecessary margins and paddings

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,25 +3,25 @@ import './About.css'
 const About = () => {
   return (
     <section className="about-wrapper" id="About">
-      <div className="paddings innerWidth about-container">
+      <div className="paddings innerWidth ">
+        <h2 className="Heading">ABOUT</h2>
         {/* left side */}
-        <div className=" about-image">
-          <img src="./frontend.jpg" alt="frontend" />
-        </div>
-        {/* right side */}
-        <div className="about">
-          <div className="about-title">
-            <h2 className="heading">ABOUT</h2>
+        <div className="about-container">
+          <div className=" about-image">
+            <img src="./frontend.jpg" alt="frontend" />
           </div>
-          <div className="about-para ">
-            <p className="secondaryText">
-              Experienced Frontend Developer proficient in HTML, CSS,
-              JavaScript, and React, Redux Toolkit, excelling in crafting intuitive user
-              interfaces. Actively broadening skill set to encompass backend
-              technologies, including Node.js, Express.js, and MongoDB.
-              Committed to ongoing learning and professional development for a
-              well-rounded expertise in web development.
-            </p>
+          {/* right side */}
+          <div className="about">
+            <div className="about-para ">
+              <p className="secondaryText">
+                Experienced Frontend Developer proficient in HTML, CSS,
+                JavaScript, and React, Redux Toolkit, excelling in crafting
+                intuitive user interfaces. Actively broadening skill set to
+                encompass backend technologies, including Node.js, Express.js,
+                and MongoDB. Committed to ongoing learning and professional
+                development for a well-rounded expertise in web development.
+              </p>
+            </div>
           </div>
         </div>
       </div>
